15dc39ea36d870066239f73ab59809cb508e4a0a
Leonardo Senff <112514561+leosenff@users.noreply.github.com>
2024-02-27 17:35:27 -0300

    Add create button to generate a new grid and change hovering to a function

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-let columns = 6;
-let rows = 6;
-
 const grid = document.querySelector('.container');
 
-for (let i = 0; i < columns; i++){
-    let column = document.createElement('div');
-    column.className ='column';
-    for (let j = 0; j < rows; j++) {
-        let cell = document.createElement('div');
-        cell.className = 'cell';
-        
-        column.appendChild(cell);
+function gridSet(gridNum){
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
-    grid.appendChild(column);
+    for (let i = 0; i < gridNum; i++){
+        let column = document.createElement('div');
+       column.className ='column';
+       for (let j = 0; j < gridNum; j++) {
+          let cell = document.createElement('div');
+         cell.className = 'cell';
+        
+         column.appendChild(cell);
+     }
+      grid.appendChild(column);
+    }
 }
 
-let hover = document.querySelectorAll(".cell");
-for (const hoverColor of hover){
-    hoverColor.addEventListener("mouseover", () =>{
-        hoverColor.style.backgroundColor = "blue";
+gridSet(16);
+hovering();
+
+function hovering(){
+    let hover = document.querySelectorAll(".cell"); 
+    for (const hoverColor of hover){
+        hoverColor.addEventListener("mouseover", () =>{
+        hoverColor.style.backgroundColor = "black";
     });
 }
+
+}
+const btn = document.querySelector("button");
+
+btn.addEventListener('click', () => {
+const userInput = window.prompt('Enter a new grid size (max 100)');
+if(isNaN(userInput)){
+    alert("Enter a valid number")
+} else {
+    gridNum = Number(userInput);
+    if (gridNum > 100){
+        gridNum = 100;
+    }
+    gridSet(gridNum);
+    hovering();
+}
+
+})
 
 // for (const hoverColor of hover){
 //     hoverColor.addEventListener("mouseout", () =>{
